3b86236b5ce0cd37c5607ac6c58821079beae186
Joel Longanecker <longjoel@users.noreply.github.com>
2026-06-08 10:28:36 -0500

    switch TURN to turns:lngnckr.tech:443 for corporate VPN compatibility

--- wwwroot/js/nosebleed-player/server-player.js
+++ wwwroot/js/nosebleed-player/server-player.js
@@ -750,7 +750,7 @@
             rtcPeer = new RTCPeerConnection({
                 iceServers: [
                     { urls: ["stun:stun.l.google.com:19302", "stun:stun1.l.google.com:19302"] },
-                    { urls: ["turn:72.62.243.69:3478?transport=tcp"], username: "nosebleed", credential: "118e21c57679b3293d7a0b9adfd90ea7" }
+                    { urls: ["turns:lngnckr.tech:443?transport=tcp"], username: "nosebleed", credential: "118e21c57679b3293d7a0b9adfd90ea7" }
                 ]
             });
             rtcPeer.addTransceiver("video", { direction: "recvonly" });
